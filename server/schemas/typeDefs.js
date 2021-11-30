beea53cc413e02fe20f5ace7d80a074b70a2a574
Umera <umeramalek796@gmail.com>
2021-11-30 00:23:18 -0800

    auth.js miodified with graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,38 +1,42 @@
-const { gql }  = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    bookCount: Int
-    savedBooks: [Book]
-}
-type Book {
-    authors: String
-    description: String
-    bookId: String
-    image: String
-    link: String 
-    title: String
-    
-}
-type Auth {
-    token: ID
-    user: User
-}
-type Query {
-    users: [User]
-    user(username: String): User
-    me: User
-}
-type Mutation {
-    loginUser(email: String, email: String, password: String): Auth
-    login(email: String, password: String): Auth
-    saveBook(authors: String, description: String, bookId: String, image: String, link: String, title: String): User
-    deleteBook(bookId: String): User
-}
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    input bookToSave {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(input: bookToSave): User
+        removeBook(bookId: String!): User
+    }
 `;
 
 module.exports = typeDefs;
